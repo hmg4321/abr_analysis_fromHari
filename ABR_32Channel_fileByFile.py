442ceb675c2ee40ad268a92e5eeffc0a3ca817b6
Hannah Ginsberg <hannah@mdgcs.com>
2020-04-01 12:42:59 -0500

    First commit

---
+++ ABR_32Channel_fileByFile.py
@@ -0,0 +1,132 @@
+from anlffr.helper import biosemi2mne as bs
+import mne
+import numpy as np
+import os
+import fnmatch
+from scipy.signal import savgol_filter as sg
+from scipy.io import savemat
+
+
+# Setup bayesian-weighted averaging
+def bayesave(x, trialdim=0, timedim=1, method='mean', smoothtrials=19):
+    ntrials = x.shape[trialdim]
+    if method == 'mean':
+        summary = x.mean(axis=trialdim) * (ntrials) ** 0.5
+    else:
+        summary = np.median(x, axis=trialdim) * (ntrials) ** 0.5
+
+    ntime = x.shape[timedim]
+    wts = 1 / np.var(x - summary, axis=timedim, keepdims=True)
+    wts = sg(wts, smoothtrials, 3, axis=trialdim)  # Smooth the variance
+    normFactor = wts.sum()
+    wts = wts.repeat(ntime, axis=timedim)
+    ave = (x * wts).sum(axis=trialdim) / normFactor
+    return ave
+
+
+# Adding Files and locations
+froot = 'D:/DATA/ABR/'
+
+# Done!
+# subjlist = ['S057', 'S064', 'S084', 'S078', 'S024', 'S026', 'S028', 'S011',
+#            'S025', 'S027', 'S034', 'S037', 'S039', 'S046', 'S043', 'S049',
+#            'S052', 'S053', 'S153', 'S150', 'S107', 'S050', 'S114',
+#            'S077', 'S059', 'S073', 'S074', 'S083', 'S155', 'S158', 'S160',
+#            'S162', 'S159', 'S061', 'S157', 'S166', 'S108', 'S052', 'S046',
+#            'S088', 'S130', 'S069', 'S147', 'S076', 'S075', 'S125', 'S091',
+#            'S051', 'S042', 'SM', 'S115', 'S134', 'S068', 'S148', 'S152',
+#            'S165', 'S172', 'S179', 'S177', 'S163', 'S060','S019', 'S020',
+#            'S077', 'S032', 'S072'.  MAYBE THE REST CHECK WHEN YOU HAVE ACCESS
+#              'S150', 'S107', 'S050', 'S114', 'S077', 
+#           'S059', 'S073', 'S074', 'S083', 'S155', 'S158']
+
+# Redo S040 -- has both High-pass and regular clicks, also S034
+
+# S162_R, S157_R, S088_R, S075_R, S068_R needs to be done separately
+
+# One big file..grr.. ignore for now: 'S111', 'S072', 'S032'
+
+# New subjects
+# S153 failed
+subjlist = ['S068', 'S088', 'S215', 'S217', 'S213']
+
+earlist = ['L', 'R']
+
+
+for subj in subjlist:
+    for ear in earlist:
+        if ear == 'L':
+            conds = [[3, 9], [5, 10]]
+            names = ['_L_soft', '_L_moderate']
+        else:
+            conds = [[48, 144], [80, 160]]
+            names = ['_R_soft', '_R_moderate']
+        #        if ear == 'L':
+        #            conds = [[6, 12], ]
+        #            names = ['_L_loud', ]
+        #        else:
+        #            conds = [[96, 192], ]
+        #            names = ['_R_loud']
+        print 'Running Subject', subj, ear, 'ear'
+        for ind, cond in enumerate(conds):
+            name = names[ind]
+            print 'Doing condition ', cond
+            fpath = froot + '/' + subj + '/'
+            bdfs = fnmatch.filter(os.listdir(fpath), subj + '_ABR*.bdf')
+
+            if len(bdfs) >= 1:
+                for k, bdf in enumerate(bdfs):
+                    edfname = fpath + bdf
+                    # Load data and read event channel
+                    extrachans = [u'GSR1', u'GSR2', u'Erg1', u'Erg2', u'Resp',
+                                  u'Plet', u'Temp']
+                    raw, eves = bs.importbdf(edfname, nchans=36,
+                                             extrachans=extrachans)
+                    raw.set_channel_types({'EXG3': 'eeg', 'EXG4': 'eeg'})
+
+                    # Pick channels to not include in epoch rejection
+                    raw.info['bads'] += ['EXG3', 'EXG4', 'A1', 'A2', 'A30',
+                                         'A7', 'A6', 'A24', 'A28', 'A29',
+                                         'A3', 'A11', 'A15', 'A16', 'A17',
+                                         'A10', 'A21', 'A20', 'A25']
+                    # Filter the data
+                    raw.filter(l_freq=130., h_freq=3000, picks=np.arange(36))
+
+                    # Epoch the data
+                    tmin, tmax = -0.002, 0.015
+                    bmin, bmax = -0.001, 0.001
+                    rejthresh = 50e-6  # Because of high-pass but using median
+                    epochs = mne.Epochs(raw, eves, cond, tmin=tmin, proj=False,
+                                        tmax=tmax, baseline=(bmin, bmax),
+                                        picks=np.arange(36),
+                                        reject=dict(eeg=rejthresh),
+                                        verbose='WARNING')
+                    xtemp = epochs.get_data()
+                    t = epochs.times * 1e3 - 1.6  # Adjust for delay and use ms
+                    # Reshaping so that channels is first
+                    if(xtemp.shape[0] > 0):
+                        xtemp = xtemp.transpose((1, 0, 2))
+                        if(k == 0):
+                            x = xtemp
+                        else:
+                            x = np.concatenate((x, xtemp), axis=1)
+                    else:
+                        continue
+            else:
+                RuntimeError('No BDF files found!!')
+
+            # Average data
+            goods = [28, 3, 30, 26, 4, 25, 7, 31, 22, 9, 8, 21, 11, 12, 18]
+            if ear == 'L':
+                refchan = 34
+            else:
+                refchan = 35
+
+            y = x[goods, :, :].mean(axis=0) - x[refchan, :, :]
+            z = np.median(y, axis=0) * 1e6  # microV
+
+            # Make dictionary and save
+            mdict = dict(t=t, x=z)
+            savepath = froot + '/ABRresults/'
+            savename = subj + name + '_ABR.mat'
+            savemat(savepath + savename, mdict)
---
+++ ABR_viz.py
@@ -0,0 +1,153 @@
+import mne
+import os
+import fnmatch
+import pylab as pl
+from warnings import warn
+import numpy as np
+from anlffr.preproc import peak_finder
+
+# Adding Files and locations
+froot = 'D:/DATA/ABR/'
+
+subjlist = ['S050', ]
+
+
+def find_wave(t, y, whichwave='I'):
+    """Automatically find a certain ABR wave (1, 3, or 5)
+
+    Parameters
+    ----------
+    t - time array (in milliseconds)
+    y - the ABR waveform (in voltage units)
+    number - 0 (SP), 1 (wave I, default), 3 (wave III), or 5 (wave V)
+
+    Returns
+    -------
+    locs, vals - Location of positive and negative end of peak and the values
+
+    Note
+    ----
+
+    Currently, the waves are defined from a peak to the following trough.
+
+    """
+    tmins = dict(I=1., SP=0., III=3., V=5.)
+    tmaxs = dict(I=3., SP=2., III=5., V=8.)
+
+    tmin = tmins[whichwave]
+    tmax = tmaxs[whichwave]
+    i1 = np.argmin(np.abs(t - tmin))
+    i2 = np.argmin(np.abs(t - tmax))
+
+    # Peak locations and values
+    plocs, pvals = peak_finder(y, (max(y) - min(y))/20.)
+    valid = np.logical_and(plocs > i1, plocs < i2)
+    plocs = plocs[valid].tolist()
+
+    # Trough locations and values
+    tlocs, tvals = peak_finder(y, (max(y) - min(y))/20., extrema=-1)
+    valid = np.logical_and(tlocs > i1, tlocs < i2)
+    tlocs = tlocs[valid].tolist()
+
+    # Sort through peaks and troughs to get adjacent pairs (P followed by T)
+    pairs = []
+    while len(plocs) > 0 and len(tlocs) > 0:
+        while len(tlocs) > 0 and tlocs[0] < plocs[0]:
+            print len(tlocs)
+            tlocs.pop(0)
+        if len(tlocs) > 0:
+            ploc = plocs.pop(0)
+            tloc = tlocs[0]
+            if len(plocs) > 0:
+                ploc_next = plocs[0]
+                if ploc_next > tloc:
+                    tloc = tlocs.pop(0)
+                    pair = [ploc, tloc]
+                    pairs = pairs + [pair]
+            else:
+                tloc = tlocs.pop(0)
+                pair = [ploc, tloc]
+                pairs = pairs + [pair]
+
+    # If there are multiple pairs return the largest peak-to-trough pair
+    if len(pairs) == 0:
+        warn('No peak-trough pair was found for requested wave')
+        locs = []
+        vals = []
+        return locs, vals
+    else:
+        if len(pairs) == 1:
+            locs = pairs[0]
+            vals = [y[locs[0]], y[locs[1]]]
+            return locs, vals
+        else:
+            warn('Multiple peak-trough pairs found. Returning the largest!')
+            p2t = np.zeros(len(pairs))
+            for pair in pairs:
+                p2t = p2t + [y[pair[0]] - y[pair[1]], ]
+            locs = pairs[np.argmax(p2t)]
+            vals = [y[locs[0]], y[locs[1]]]
+            return locs, vals
+
+rebase = True
+for subj in subjlist:
+
+    fpath = froot + '/' + subj + '/'
+
+    print 'Visualizing Subject', subj
+
+    avefiles = fnmatch.filter(os.listdir(fpath), subj + '_ABR*-ave.fif')
+
+    if len(avefiles) == 1:
+        abrs = mne.read_evokeds(fpath + avefiles[0])
+    else:
+        RuntimeError('No ABR average files found!! OR too many files')
+
+    goods = [28, 3, 30, 26, 4, 25, 7, 31, 22, 9, 8, 21, 11, 12, 18]
+
+    # Plot data
+    R = [3, 4, 5]
+    L = [0, 1, 2]
+    labels = ['85 dB peSPL', '100 dB peSPL', '115 dB peSPL']
+    for ind, k in enumerate(L):
+        abr = abrs[k]
+        x = abr.data * 1e6  # microV
+        t = abr.times * 1e3 - 1.6  # Adjust for delay and use milliseconds
+        y = x[goods, :].mean(axis=0) - x[34, :]
+        if rebase is True:
+            y = y - y[(t > -2.) & (t < 0.)].mean()
+        pl.plot(t, y, linewidth=2, label=labels[ind])
+        locs, vals = find_wave(t, y, whichwave='I')
+        # pl.hold(True)
+        pl.plot(t[locs], y[locs], 'ro', markersize=6)
+    pl.xlabel('Time (ms)', fontsize=14)
+    pl.ylabel('ABR (uV)', fontsize=14)
+    pl.title('Left Ear', fontsize=14)
+    pl.xlim((-2., 13.4))
+    pl.ylim((-1.0, 2.))
+    ax = pl.gca()
+    ax.tick_params(labelsize=14)
+    pl.legend()
+    pl.show()
+
+    pl.figure()
+    for ind, k in enumerate(R):
+        abr = abrs[k]
+        x = abr.data * 1e6  # microV
+        t = abr.times * 1e3 - 1.6  # Adjust for delay and use milliseconds
+        y = x[goods, :].mean(axis=0) - x[35, :]
+        if rebase is True:
+            y = y - y[(t > -2.) & (t < 0.)].mean()
+        pl.plot(t, y, linewidth=2, label=labels[ind])
+        locs, vals = find_wave(t, y, whichwave='I')
+        # pl.hold(True)
+        pl.plot(t[locs], y[locs], 'ro', markersize=6)
+    pl.xlabel('Time (ms)', fontsize=16)
+    pl.ylabel('ABR (uV)', fontsize=16)
+    pl.title('Right Ear', fontsize=20)
+    pl.xlim((-2., 13.4))
+    pl.ylim((-1., 2.))
+    ax = pl.gca()
+    ax.tick_params(labelsize=14)
+    pl.legend()
+    pl.show()
